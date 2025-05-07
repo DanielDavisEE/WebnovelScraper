963bccaf0e11e4f65a06999180db860c3b33ca6e
DanielDavis <danieldavis998@gmail.com>
2025-05-08 00:49:03 +1200

    remove old code

--- src/webnovels/editing.py
+++ src/webnovels/editing.py
@@ -8,114 +8,6 @@ from spellchecker import SpellChecker
 
 from webnovels.utils import NOVELS_DIR, get_chapter_index, get_file_safe, get_novel_dir
 
-GLOBAL_RESOURCES = Path(__file__).parent / '.resources'
-
-
-# @contextmanager
-# def get_resources(resource_dir: Path):
-#     assert resource_dir.name == '.resources'
-#
-#     with open(resource_dir / 'dictionary_ext.txt', 'r') as dictionary_file:
-#         dictionary_ext = dictionary_file.readlines()
-#     with open(resource_dir / 'word_swaps.json', 'r') as word_swaps_file:
-#         word_swaps = json.load(word_swaps_file)
-#
-#     try:
-#         yield dictionary_ext, word_swaps
-#
-#     finally:
-#         with open(resource_dir / 'dictionary_ext.txt', 'r') as dictionary_file:
-#             dictionary_file.write('\n'.join(dictionary_ext))
-#         with open(resource_dir / 'word_swaps.json', 'r') as word_swaps_file:
-#             json.dump(word_swaps, word_swaps_file, indent=2)
-
-def tokenize(text):
-    tokens = []
-    current_token = ''
-    i = 0
-    punctuation = set('.,!?;:"\'()[]{} -–—…\n‘’“”')
-    strip_chars = set('.,!?;:"\'()[]{}…‘’“”')
-
-    while i < len(text):
-        char = text[i]
-
-        if char.isspace():
-            if current_token:
-                tokens.append(current_token)
-                current_token = ''
-            i += 1
-        elif char in punctuation:
-            if current_token:
-                tokens.append(current_token)
-                current_token = ''
-            # Handle paired quotes as separate tokens
-            tokens.append(char)
-            i += 1
-        else:
-            current_token += char
-            i += 1
-
-    if current_token:
-        tokens.append(current_token)
-
-    return tokens
-
-
-def correct_token(token, spell_checker):
-    isolated_word = token
-
-    # Special case: HTML tags
-    if '<' in token and '>' in token:
-        isolated_word = ''.join(re.split(r'<[\/a-z]{,5}>', isolated_word))
-
-    punctuation = '.,!?;:"\'()[]{} -–—…\n‘’“”'
-    strip_chars = '.,!?;:"\'()[]{}…‘’“”'
-    isolated_word = isolated_word.strip(punctuation)
-
-    if isolated_word:
-        misspelt_word = spell_checker.unknown([isolated_word])
-        if misspelt_word:
-            misspelt_word = misspelt_word.pop()
-            possible_corrections = list(spell_checker.candidates(misspelt_word))
-            print(f'Identified spelling error: {misspelt_word}')
-            print('Options:')
-            print(' 1. Add to global dict')
-            print(' 2. Add to novel dict')
-            print(' 3. Custom correction')
-            for i, correction in enumerate(possible_corrections, start=4):
-                print(f' {i}. {correction}')
-            answer = int(input(''))
-            if answer in [1, 2]:
-                correct_word = misspelt_word
-            elif answer == 3:
-                correct_word = input('Custom correction: ')
-            elif answer - 4 < len(possible_corrections):
-                correct_word = possible_corrections[answer - 4]
-            else:
-                raise RuntimeError
-        else:
-            correct_word = isolated_word
-    else:
-        correct_word = ''
-
-    return token.replace(isolated_word, correct_word)
-
-
-def edit_chapter(novel_dir, fname, spell_checker):
-    with open(novel_dir / 'raw_chapters' / fname) as raw_chapter_file:
-        raw_chapter = raw_chapter_file.read()
-
-    processed_chapter = []
-    initial_spaces = raw_chapter.count(' ')
-    for token in raw_chapter.split(' '):
-        processed_chapter.append(correct_token(token, spell_checker))
-    processed_chapter = ' '.join(processed_chapter)
-
-    if initial_spaces != processed_chapter.count(' '):
-        raise RuntimeError('Discrepancy in number of spaces')
-    return processed_chapter
-
-
 ChangeRecord = dict[str, int | str]
 
 
@@ -123,7 +15,7 @@ class NovelSpellChecker:
     """
 
     """
-    GLOBAL_DICT_EXT = GLOBAL_RESOURCES / 'dictionary_ext.txt'
+    GLOBAL_DICT_EXT = Path(__file__).parent / '.resources' / 'dictionary_ext.txt'
 
     def __init__(self, novel_title):
         self.log = logging.getLogger(self.__class__.__name__)
